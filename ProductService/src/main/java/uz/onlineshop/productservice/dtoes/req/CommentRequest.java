2bead9139a59dc9a7d349954d594510931fd1c81
TurgunovX <xturgunov069@gmail.com>
2025-07-24 22:55:37 +0500

    order service config fixed

--- ProductService/src/main/java/uz/onlineshop/productservice/dtoes/req/CommentRequest.java
+++ ProductService/src/main/java/uz/onlineshop/productservice/dtoes/req/CommentRequest.java
@@ -1,10 +1,10 @@
-package uz.onlineshop.productservice.dtoes.req;
-
-import lombok.Data;
-
-@Data
-public class CommentRequest {
-    private Long productId;
-    private String text;
-    private Integer rating;
-}
+//package uz.onlineshop.productservice.dtoes.req;
+//
+//import lombok.Data;
+//
+//@Data
+//public class CommentRequest {
+//    private Long productId;
+//    private String text;
+//    private Integer rating;
+//}
