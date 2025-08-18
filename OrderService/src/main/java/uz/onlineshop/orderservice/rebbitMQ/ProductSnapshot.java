7d40a1b855c666a94ac97aa5ff87e1cce0e6a80d
TurgunovX <xturgunov069@gmail.com>
2025-08-18 13:13:20 +0500

    rebbit mq olindi

---
+++ OrderService/src/main/java/uz/onlineshop/orderservice/rebbitMQ/ProductSnapshot.java
@@ -0,0 +1,35 @@
+//// ProductSnapshot.java
+//package uz.onlineshop.orderservice.rebbitMQ;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import java.math.BigDecimal;
+//
+//@Entity
+//@Table(name = "product_snapshots")
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class ProductSnapshot {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String productName;
+//
+//    private String description;
+//
+//    private BigDecimal price;
+//
+//    private String color;
+//
+//    private String imageUrl;
+//
+//    private Integer stockQuantity;
+//
+//    private Long categoryId;
+//}
